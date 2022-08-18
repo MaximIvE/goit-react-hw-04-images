74f792e8914a51e42cba0dbf711ea22ea4dce9e4
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-08-18 15:25:08 +0300

    fix Modal: delete useState

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { createPortal } from 'react-dom';
 import { Overlay, ModalContetn } from './Modal.styled';
 import PropTypes from 'prop-types';
